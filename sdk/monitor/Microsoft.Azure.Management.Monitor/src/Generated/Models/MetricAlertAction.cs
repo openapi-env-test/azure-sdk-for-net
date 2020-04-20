4f062aa4b62dcce2a91b33419d3e2effd56fe8d0
SDK Automation <sdkautomation@microsoft.com>
2020-04-20 05:49:16 +0000

    Update from master

--- sdk/monitor/Microsoft.Azure.Management.Monitor/src/Generated/Models/MetricAlertAction.cs
+++ sdk/monitor/Microsoft.Azure.Management.Monitor/src/Generated/Models/MetricAlertAction.cs
@@ -33,12 +33,12 @@ namespace Microsoft.Azure.Management.Monitor.Models
         /// </summary>
         /// <param name="actionGroupId">the id of the action group to
         /// use.</param>
-        /// <param name="webHookProperties">The properties of a webhook
+        /// <param name="webhookProperties">The properties of a webhook
         /// object.</param>
-        public MetricAlertAction(string actionGroupId = default(string), IDictionary<string, string> webHookProperties = default(IDictionary<string, string>))
+        public MetricAlertAction(string actionGroupId = default(string), IDictionary<string, string> webhookProperties = default(IDictionary<string, string>))
         {
             ActionGroupId = actionGroupId;
-            WebHookProperties = webHookProperties;
+            WebhookProperties = webhookProperties;
             CustomInit();
         }
 
@@ -56,8 +56,8 @@ namespace Microsoft.Azure.Management.Monitor.Models
         /// <summary>
         /// Gets or sets the properties of a webhook object.
         /// </summary>
-        [JsonProperty(PropertyName = "webHookProperties")]
-        public IDictionary<string, string> WebHookProperties { get; set; }
+        [JsonProperty(PropertyName = "webhookProperties")]
+        public IDictionary<string, string> WebhookProperties { get; set; }
 
     }
 }
--- sdk/monitor/Microsoft.Azure.Management.Monitor/src/generate.ps1
+++ sdk/monitor/Microsoft.Azure.Management.Monitor/src/generate.ps1
@@ -1,1 +1,1 @@
-Start-AutoRestCodeGeneration -ResourceProvider "monitor/resource-manager" -AutoRestVersion "v2"
+Start-AutoRestCodeGeneration -ResourceProvider "monitor/resource-manager" -AutoRestVersion "latest"
