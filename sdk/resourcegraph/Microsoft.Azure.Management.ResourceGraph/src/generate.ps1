805dd2ab3323888c5635c751fccda26265103625
SDK Automation <sdkautomation@microsoft.com>
2020-04-22 05:25:22 +0000

    Update from master

--- sdk/resourcegraph/Microsoft.Azure.Management.ResourceGraph/src/generate.ps1
+++ sdk/resourcegraph/Microsoft.Azure.Management.ResourceGraph/src/generate.ps1
@@ -1,1 +1,1 @@
-Start-AutoRestCodeGeneration -ResourceProvider "resourcegraph/resource-manager" -AutoRestVersion "v2"
+Start-AutoRestCodeGeneration -ResourceProvider "resourcegraph/resource-manager" -AutoRestVersion "latest"
