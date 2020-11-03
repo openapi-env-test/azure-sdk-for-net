9989cb272643e4ddb93e3a98d148e67b6862ee12
Shivangi Reja <shreja@microsoft.com>
2020-11-03 15:20:15 -0800

    Add generate out functionality

--- eng/scripts/Sdk-Generate.ps1
+++ eng/scripts/Sdk-Generate.ps1
@@ -1,25 +1,65 @@
-param($GenerateInput)
+param($GenerateInput, $GenerateOutput)
 
 # Get the list of changed swaggers
 $input = Get-Content $GenerateInput | ConvertFrom-Json
 $inputFiles = $input.changedFiles;
 $inputFiles += $input.relatedReadmeMdFiles;
-Write-Output "List Of changed swagger files and related readmes" $inputFiles
+Write-Output "List Of changed swagger files and related readmes" $inputFiles "`n"
 $headSha = $input.headSha
-Write-Host
 
 $autorestFilesPath = Get-ChildItem -Path "./sdk"  -Filter autorest.md -Recurse
 
 Write-Host "Updating autorest.md for the changed swaggers..."
+$sdkPaths = @()
 foreach ($inputFile in $inputFiles)
 {
     foreach ($path in $autorestFilesPath)
     {
-        (Get-Content -Raw $path) -replace "[\/][0-9a-f]{4,40}[\/]$inputFile", "/$headSha/$inputFile" | `
+     $fileContent = Get-Content -Raw $path
+     $containsUrl = $fileContent | %{$_ -match "[\/][0-9a-f]{4,40}[\/]$inputFile"}
+     if ($containsUrl -contains $true)
+     {
+       Write-Host "Updating " $path
+       $fileContent -replace "[\/][0-9a-f]{4,40}[\/]$inputFile", "/$headSha/$inputFile" | `
          Set-Content $path -NoNewline
+
+       $sdkPaths += (get-item $path).Directory.Parent.FullName
+     }
     }
 }
-Write-Host "autorest.md files updated"
+Write-Host "autorest.md files are updated. `n"
 
 dotnet msbuild /restore /t:GenerateCode "./eng/service.proj"
 
+$packages = @()
+foreach ($sdkPath in $sdkPaths)
+{
+    $srcPath = Join-Path $sdkPath 'src'
+    $csprojPath = Get-ChildItem $srcPath -Filter *.csproj -Recurse
+    $artifactsPath = "./artifacts/packages"
+    If(!(test-path $artifactsPath))
+    {
+      New-Item -ItemType Directory -Force -Path $artifactsPath
+    }
+    dotnet pack $csprojPath --output $artifactsPath
+
+    $packageName = Split-Path $sdkPath -Leaf
+    $readmePath = Join-Path $sdkPath 'readme.md'
+    $artifacts = @()
+    $artifacts +=  Get-ChildItem $artifactsPath -Filter *$packageName* -Recurse | Select-Object -ExpandProperty FullName
+    $packageInfo = [PSCustomObject]@{
+        packageName = $packageName
+        path = $sdkPath
+        readmeMd = $readmePath
+        artifacts = $artifacts
+    }
+
+    $packages += $packageInfo
+}
+
+if ($GenerateOutput) {
+    Write-Host "Generating output JSON..."
+    ConvertTo-Json @{
+      packages         = $packages
+    } -depth 5 | Out-File -FilePath $GenerateOutput
+}
