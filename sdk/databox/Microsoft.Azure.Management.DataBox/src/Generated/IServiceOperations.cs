12e0abca28f2589392fdc7309e392072dc7ad657
SDKAuto <sdkautomation@microsoft.com>
2020-11-10 02:48:20 +0000

    CodeGen from PR 865 in openapi-env-test/azure-rest-api-specs Test delete operation (#865)

--- sdk/databox/Microsoft.Azure.Management.DataBox/src/Generated/IServiceOperations.cs
+++ sdk/databox/Microsoft.Azure.Management.DataBox/src/Generated/IServiceOperations.cs
@@ -24,35 +24,6 @@ namespace Microsoft.Azure.Management.DataBox
     public partial interface IServiceOperations
     {
         /// <summary>
-        /// This method provides the list of available skus for the given
-        /// subscription, resource group and location.
-        /// </summary>
-        /// <param name='resourceGroupName'>
-        /// The Resource Group Name
-        /// </param>
-        /// <param name='location'>
-        /// The location of the resource
-        /// </param>
-        /// <param name='availableSkuRequest'>
-        /// Filters for showing the available skus.
-        /// </param>
-        /// <param name='customHeaders'>
-        /// The headers that will be added to request.
-        /// </param>
-        /// <param name='cancellationToken'>
-        /// The cancellation token.
-        /// </param>
-        /// <exception cref="ApiErrorException">
-        /// Thrown when the operation returned an invalid status code
-        /// </exception>
-        /// <exception cref="Microsoft.Rest.SerializationException">
-        /// Thrown when unable to deserialize the response
-        /// </exception>
-        /// <exception cref="Microsoft.Rest.ValidationException">
-        /// Thrown when a required parameter is null
-        /// </exception>
-        Task<AzureOperationResponse<IPage<SkuInformation>>> ListAvailableSkusByResourceGroupWithHttpMessagesAsync(string resourceGroupName, string location, AvailableSkuRequest availableSkuRequest, Dictionary<string, List<string>> customHeaders = null, CancellationToken cancellationToken = default(CancellationToken));
-        /// <summary>
         /// [DEPRECATED NOTICE: This operation will soon be removed]. This
         /// method validates the customer shipping address and provide
         /// alternate addresses if any.
@@ -196,28 +167,5 @@ namespace Microsoft.Azure.Management.DataBox
         /// Thrown when a required parameter is null
         /// </exception>
         Task<AzureOperationResponse<RegionConfigurationResponse>> RegionConfigurationByResourceGroupWithHttpMessagesAsync(string resourceGroupName, string location, ScheduleAvailabilityRequest scheduleAvailabilityRequest = default(ScheduleAvailabilityRequest), TransportAvailabilityRequest transportAvailabilityRequest = default(TransportAvailabilityRequest), Dictionary<string, List<string>> customHeaders = null, CancellationToken cancellationToken = default(CancellationToken));
-        /// <summary>
-        /// This method provides the list of available skus for the given
-        /// subscription, resource group and location.
-        /// </summary>
-        /// <param name='nextPageLink'>
-        /// The NextLink from the previous successful call to List operation.
-        /// </param>
-        /// <param name='customHeaders'>
-        /// The headers that will be added to request.
-        /// </param>
-        /// <param name='cancellationToken'>
-        /// The cancellation token.
-        /// </param>
-        /// <exception cref="ApiErrorException">
-        /// Thrown when the operation returned an invalid status code
-        /// </exception>
-        /// <exception cref="Microsoft.Rest.SerializationException">
-        /// Thrown when unable to deserialize the response
-        /// </exception>
-        /// <exception cref="Microsoft.Rest.ValidationException">
-        /// Thrown when a required parameter is null
-        /// </exception>
-        Task<AzureOperationResponse<IPage<SkuInformation>>> ListAvailableSkusByResourceGroupNextWithHttpMessagesAsync(string nextPageLink, Dictionary<string, List<string>> customHeaders = null, CancellationToken cancellationToken = default(CancellationToken));
     }
 }
