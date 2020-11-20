01397b9a81576b7dc163a6935046bf7e4f6f4914
Shivangi Reja <shreja@microsoft.com>
2020-11-20 10:56:57 -0800

    Add init script in swagger_to_sdk_config

--- eng/scripts/Sdk-init.ps1
+++ eng/scripts/Sdk-init.ps1
@@ -1,5 +1,5 @@
 $RepoRoot = ${PSScriptRoot}
-Write-Host "Testing path $RepoRoot"
+Write-Host "Testing init script path $RepoRoot"
 
-Get-Help .\dotnet-install.ps1
-./dotnet-install.ps1 -Version 5.0.100
+Get-Help ./dotnet-install.ps1
+& "$PSScriptRoot/dotnet-install.ps1" -Version 5.0.100
