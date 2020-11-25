1eae8824f822520bfbd6524b281a74f381cf39e4
Shivangi Reja <shreja@microsoft.com>
2020-11-24 20:37:00 -0800

    Update script

--- eng/scripts/Sdk-Generate.ps1
+++ eng/scripts/Sdk-Generate.ps1
@@ -4,63 +4,54 @@ param(
   [string] $RepoRoot = "${PSScriptRoot}/../.."
 )
 
-$input = Get-Content $InputJsonPath | ConvertFrom-Json
-$inputFilePaths = $input.changedFiles;
-$inputFilePaths += $input.relatedReadmeMdFiles;
-$inputFilePaths = $inputFilePaths | select -Unique
+$inputFileContent = Get-Content $InputJsonPath | ConvertFrom-Json
+$inputFilePaths = $inputFileContent.changedFiles;
+$inputFilePaths += $inputFileContent.relatedReadmeMdFiles;
+$inputFilePaths = $inputFilePaths | Select-Object -Unique
+
 $changedFilePaths = $inputFilePaths -join "`n";
-Write-Host "List Of changed swagger files and related readmes`n $changedFilePaths `n"
+Write-Host "List of changed swagger files and related readmes:`n$changedFilePaths`n"
 
-$autorestFilesPath = Get-ChildItem -Path "$RepoRoot/sdk"  -Filter autorest.md -Recurse | Resolve-Path -Relative
+$autorestFilesPath = Get-ChildItem -Path "$RepoRoot/sdk"  -Filter autorest.md -Recurse | Resolve-Path -Relative
 
-Write-Host "Updating autorest.md files for all the changed swaggers."
+Write-Host "Updating autorest.md files for all the changed swaggers."
 $sdksInfo = @{}
-$headSha = $input.headSha
-$repoHttpsUrl = $input.repoHttpsUrl
-foreach ($inputFilePath in $inputFilePaths)
-{
-  foreach ($path in $autorestFilesPath)
-  {
+$headSha = $inputFileContent.headSha
+$repoHttpsUrl = $inputFileContent.repoHttpsUrl
+foreach ($inputFilePath in $inputFilePaths) {
+  foreach ($path in $autorestFilesPath) {
     $fileContent = Get-Content $path
     $isUpdatedLines = $false
-    $updatedLines = foreach($line in $fileContent)
-    {
+    $updatedLines = foreach ($line in $fileContent) {
       $regexForMatchingShaAndPath = "[\/][0-9a-f]{4,40}[\/]$inputFilePath"
-      if($line -match $regexForMatchingShaAndPath)
-      {
-        $line -replace "https:\/\/[^`"]*$regexForMatchingShaAndPath", "$repoHttpsUrl/blob/$headSha/$inputFilePath"
+      if ($line -match $regexForMatchingShaAndPath) {
+        $line -replace "https:\/\/[^`"]*$regexForMatchingShaAndPath", "$repoHttpsUrl/blob/$headSha/$inputFilePath"
 
         $isUpdatedLines = $true
         $sdkpath = (get-item $path).Directory.Parent.FullName | Resolve-Path -Relative
-        if (!$sdksInfo.ContainsKey($sdkpath))
-        {
-          $specReadmePath = $input.relatedReadmeMdFiles -match $inputFilePath
+        if (!$sdksInfo.ContainsKey($sdkpath)) {
+          $specReadmePath = $inputFileContent.relatedReadmeMdFiles -match $inputFilePath
           $sdksInfo.Add($sdkpath, $specReadmePath)
         }
       }
-      else
-      {
+      else {
         $line
       }
     }
-    if($isUpdatedLines)
-    {
+    if ($isUpdatedLines) {
       $updatedLines | Out-File -FilePath $path
     }
   }
 }
 
-Write-Host "Updated autorest.md files for all the changed swaggers. `n"
+Write-Host "Updated autorest.md files for all the changed swaggers. `n"
 
-function Test-PreviousScript() 
-{
+function Test-PreviousScript() {
   $result = $null
-  if($LASTEXITCODE -eq 0)
-  {
+  if ($LASTEXITCODE -eq 0) {
     $result = "succeeded"
   }
-  else 
-  {
+  else {
     $result = "failed"
   }
   Write-Host "Result: $result"
@@ -68,10 +59,9 @@ function Test-PreviousScript() 
 }
 
 $packages = @()
-foreach ($sdkPath in $sdksInfo.Keys)
-{
+foreach ($sdkPath in $sdksInfo.Keys) {
   $packageName = Split-Path $sdkPath -Leaf
-  Write-Host "Generating code for " $packageName
+  Write-Host "Generating code for " $packageName
   $srcPath = Join-Path $sdkPath 'src'
   dotnet msbuild /restore /t:GenerateCode $srcPath
 
@@ -79,32 +69,27 @@ foreach ($sdkPath in $sdksInfo.Keys)
   $hasBreakingChange = $null
   $content = $null
   $result = Test-PreviousScript
-  if($result -eq "succeeded")
-  {
-    Write-Host "Successfully generated code for" $packageName "`n"
+  if ($result -eq "succeeded") {
+    Write-Host "Successfully generated code for" $packageName "`n"
     
     $csprojPath = Get-ChildItem $srcPath -Filter *.csproj -Recurse
     dotnet pack $csprojPath /p:RunApiCompat=$false
     $result = Test-PreviousScript
-    if($result -eq "succeeded")
-    {
+    if ($result -eq "succeeded") {
       $artifactsPath = "$RepoRoot/artifacts/packages/Debug/$packageName"
-      $artifacts +=  Get-ChildItem $artifactsPath -Filter *.nupkg -Recurse | Select-Object -ExpandProperty FullName | Resolve-Path -Relative
+      $artifacts += Get-ChildItem $artifactsPath -Filter *.nupkg -Recurse | Select-Object -ExpandProperty FullName | Resolve-Path -Relative
 
       $logFilePath = Join-Path "$srcPath" 'log.txt'
-      if(!(Test-Path $logFilePath))
-      {
+      if (!(Test-Path $logFilePath)) {
         New-Item $logFilePath
       }
       dotnet build $csprojPath /t:RunApiCompat /p:TargetFramework=netstandard2.0 /flp:v=m`;LogFile=$logFilePath
       
       $result = Test-PreviousScript
-      if($result -eq "succeeded")
-      {
+      if ($result -eq "succeeded") {
         $hasBreakingChange = $false
       }
-      else
-      {
+      else {
         $logFile = Get-Content -Path $logFilePath | select-object -skip 2
         $breakingChanges = $logFile -join ",`n"
         $content = "Breaking Changes: $breakingChanges"
@@ -112,15 +97,13 @@ foreach ($sdkPath in $sdksInfo.Keys)
         $result = "succeeded"
       }
 
-      if (Test-Path $logFilePath) 
-      {
+      if (Test-Path $logFilePath) {
         Remove-Item $logFilePath
       }
     }
   } 
-  else 
-  {
-    Write-Host "Error occurred while generating code for" $packageName "`n"
+  else {
+    Write-Error "Error occurred while generating code for" $packageName "`n"
   }
 
   $path = @()
@@ -130,32 +113,32 @@ foreach ($sdkPath in $sdksInfo.Keys)
   $readmeMd += $sdksInfo[$sdkPath]
 
   $changelog = [PSCustomObject]@{
-    content = $content
+    content           = $content
     hasBreakingChange = $hasBreakingChange
-    }
+  }
 
-  $downloadUrlPrefix = $input.installInstructionInput.downloadUrlPrefix
+  $downloadUrlPrefix = $inputFileContent.installInstructionInput.downloadUrlPrefix
   $fileName = Split-Path $artifacts[0] -Leaf
   $installInstructions = [PSCustomObject]@{
     full = "Download the $packageName package from [here]($downloadUrlPrefix/$fileName)"
     lite = "Download the $packagename package from [here]($downloadUrlPrefix/$fileName)"
-    }
+  }
 
   $packageInfo = [PSCustomObject]@{
-    packageName = $packageName
-      path = $path
-      readmeMd = $readmeMd
-      changelog = $changelog
-      artifacts = $artifacts
-      installInstructions = $installInstructions
-      result = $result
-      }
-    $packages += $packageInfo
+    packageName         = $packageName
+    path                = $path
+    readmeMd            = $readmeMd
+    changelog           = $changelog
+    artifacts           = $artifacts
+    installInstructions = $installInstructions
+    result              = $result
+  }
+  $packages += $packageInfo
 }
 
 if ($OutputJsonPath) {
-    Write-Host "`nGenerating output JSON..."
-    ConvertTo-Json @{
-      packages         = $packages
-    } -depth 5 | Out-File -FilePath $OutputJsonPath
+  Write-Host "`nGenerating output JSON..."
+  ConvertTo-Json @{
+    packages = $packages
+  } -depth 5 | Out-File -FilePath $OutputJsonPath
 }
--- eng/scripts/Sdk-init.ps1
+++ eng/scripts/Sdk-init.ps1
@@ -1,5 +1,44 @@
-$RepoRoot = ${PSScriptRoot}
-Write-Host "Testing init script path $RepoRoot"
+[string] $RepoRoot = "${PSScriptRoot}/../.."
+[string] $dotnetInstallScriptVersion = "v1"
 
-Get-Help ./dotnet-install.ps1
-& "$PSScriptRoot/dotnet-install.ps1" -Version 5.0.100
+function GetDotNetInstallScript() {
+    $installScript = Join-Path $RepoRoot 'dotnet-install.ps1'
+    if (!(Test-Path $installScript)) {
+        New-Item -Path $RepoRoot -Force -ItemType 'Directory' | Out-Null
+        $maxRetries = 5
+        $retries = 1
+       
+        $uri = "https://dot.net/$dotnetInstallScriptVersion/dotnet-install.ps1"
+        while ($true) {
+            try {
+                Write-Host "GET $uri"
+                Invoke-WebRequest $uri -OutFile $installScript
+                break
+            }
+            catch {
+                Write-Host "Failed to download '$uri'"
+                Write-Error $_.Exception.Message -ErrorAction Continue
+            }
+            if (++$retries -le $maxRetries) {
+                $delayInSeconds = [math]::Pow(2, $retries) - 1 # Exponential backoff
+                Write-Host "Retrying. Waiting for $delayInSeconds seconds before next attempt ($retries of $maxRetries)."
+                Start-Sleep -Seconds $delayInSeconds
+            }
+            else {
+                throw "Unable to download file in $maxRetries attempts."
+            }
+        }
+    }
+
+    return $installScript
+}
+
+$GlobalJson = Get-Content -Raw -Path (Join-Path $RepoRoot 'global.json') | ConvertFrom-Json
+$dotnetSdkVersion = $GlobalJson.sdk.version
+
+$installScript = GetDotNetInstallScript
+& $installScript -Version $dotnetSdkVersion
+
+if (Test-Path $installScript) {
+    Remove-Item $installScript
+}
