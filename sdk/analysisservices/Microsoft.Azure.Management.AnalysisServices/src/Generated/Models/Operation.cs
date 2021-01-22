8fec33a6a8ab1c1fc8bdef7be7371fbf4012b441
SDKAuto <sdkautomation@microsoft.com>
2021-01-22 02:34:33 +0000

    CodeGen from PR 1005 in openapi-env-test/azure-rest-api-specs Merge d4a96142b27c7d6740eaa7bbbd8bc295ebf8f56e into dad59855f09aefb9ffc4a1ab8558bff23e05e06f

--- sdk/analysisservices/Microsoft.Azure.Management.AnalysisServices/src/Generated/Models/Operation.cs
+++ sdk/analysisservices/Microsoft.Azure.Management.AnalysisServices/src/Generated/Models/Operation.cs
@@ -33,10 +33,12 @@ namespace Microsoft.Azure.Management.Analysis.Models
         /// {provider}/{resource}/{operation}.</param>
         /// <param name="display">The object that represents the
         /// operation.</param>
-        public Operation(string name = default(string), OperationDisplay display = default(OperationDisplay))
+        /// <param name="origin">The origin</param>
+        public Operation(string name = default(string), OperationDisplay display = default(OperationDisplay), string origin = default(string))
         {
             Name = name;
             Display = display;
+            Origin = origin;
             CustomInit();
         }
 
@@ -56,6 +58,12 @@ namespace Microsoft.Azure.Management.Analysis.Models
         /// </summary>
         [JsonProperty(PropertyName = "display")]
         public OperationDisplay Display { get; set; }
+
+        /// <summary>
+        /// Gets the origin
+        /// </summary>
+        [JsonProperty(PropertyName = "origin")]
+        public string Origin { get; private set; }
 
     }
 }
