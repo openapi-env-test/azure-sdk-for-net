b4a41692690e040b48660e9a36917ef9468afb3e
SDKAuto <sdkautomation@microsoft.com>
2021-01-22 03:34:32 +0000

    CodeGen from PR 1032 in openapi-env-test/azure-rest-api-specs Merge 24a6b3588dc07723c175d2e86fc02e5470a88a68 into a00d94e210f6385fe93aa0d4e6f9a0f9480964f7

--- sdk/advisor/Microsoft.Azure.Management.Advisor/src/Generated/IRecommendationMetadataOperations.cs
+++ sdk/advisor/Microsoft.Azure.Management.Advisor/src/Generated/IRecommendationMetadataOperations.cs
@@ -35,7 +35,7 @@ namespace Microsoft.Azure.Management.Advisor
         /// <param name='cancellationToken'>
         /// The cancellation token.
         /// </param>
-        /// <exception cref="Microsoft.Rest.Azure.CloudException">
+        /// <exception cref="ArmErrorResponseException">
         /// Thrown when the operation returned an invalid status code
         /// </exception>
         /// <exception cref="Microsoft.Rest.SerializationException">
@@ -54,7 +54,7 @@ namespace Microsoft.Azure.Management.Advisor
         /// <param name='cancellationToken'>
         /// The cancellation token.
         /// </param>
-        /// <exception cref="Microsoft.Rest.Azure.CloudException">
+        /// <exception cref="ArmErrorResponseException">
         /// Thrown when the operation returned an invalid status code
         /// </exception>
         /// <exception cref="Microsoft.Rest.SerializationException">
@@ -76,7 +76,7 @@ namespace Microsoft.Azure.Management.Advisor
         /// <param name='cancellationToken'>
         /// The cancellation token.
         /// </param>
-        /// <exception cref="Microsoft.Rest.Azure.CloudException">
+        /// <exception cref="ArmErrorResponseException">
         /// Thrown when the operation returned an invalid status code
         /// </exception>
         /// <exception cref="Microsoft.Rest.SerializationException">
