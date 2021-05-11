d97f4cd2682edd7d97e2cf0a1b9621552b7753b3
SDKAuto <sdkautomation@microsoft.com>
2021-05-11 05:06:43 +0000

    CodeGen from PR 2608 in openapi-env-test/azure-rest-api-specs Merge 1adc6524b583bc1d49c1aeb878d2c0a04345888d into 076d058ef58d1f88109f7b739c6477b62efe5131

--- sdk/securityinsights/Microsoft.Azure.Management.SecurityInsights/src/Generated/Models/DataConnector.cs
+++ sdk/securityinsights/Microsoft.Azure.Management.SecurityInsights/src/Generated/Models/DataConnector.cs
@@ -10,11 +10,13 @@
 
 namespace Microsoft.Azure.Management.SecurityInsights.Models
 {
+    using Newtonsoft.Json;
     using System.Linq;
 
     /// <summary>
     /// Data connector.
     /// </summary>
+    [Newtonsoft.Json.JsonObject("DataConnector")]
     public partial class DataConnector : ResourceWithEtag
     {
         /// <summary>
