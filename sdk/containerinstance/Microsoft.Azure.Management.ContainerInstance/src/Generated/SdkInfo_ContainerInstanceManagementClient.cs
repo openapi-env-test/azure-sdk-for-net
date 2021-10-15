cfd7f18513d8b6b0edd43830e4367ea7262be573
SDKAuto <sdkautomation@microsoft.com>
2021-10-15 07:30:00 +0000

    CodeGen from PR 2993 in openapi-env-test/azure-rest-api-specs Merge df7004bc64c478de1fe9cf886b490019e59955b7 into 210b6d92b367c0b4f0495012685f53d6219dc02e

--- sdk/containerinstance/Microsoft.Azure.Management.ContainerInstance/src/Generated/SdkInfo_ContainerInstanceManagementClient.cs
+++ sdk/containerinstance/Microsoft.Azure.Management.ContainerInstance/src/Generated/SdkInfo_ContainerInstanceManagementClient.cs
@@ -26,16 +26,5 @@ namespace Microsoft.Azure.Management.ContainerInstance
               }.AsEnumerable();
           }
       }
-      // BEGIN: Code Generation Metadata Section
-      public static readonly String AutoRestVersion = "v2";
-      public static readonly String AutoRestBootStrapperVersion = "autorest@3.4.1";
-      public static readonly String AutoRestCmdExecuted = "cmd.exe /c autorest.cmd https://github.com/Azure/azure-rest-api-specs/blob/master/specification/containerinstance/resource-manager/readme.md --csharp --version=v2 --reflect-api-versions --csharp-sdks-folder=C:\\Users\\josephporter\\source\\azure-sdk-for-net\\sdk";
-      public static readonly String GithubForkName = "Azure";
-      public static readonly String GithubBranchName = "master";
-      public static readonly String GithubCommidId = "314f28163917b9cfc527f7776b5e4a1dea69d295";
-      public static readonly String CodeGenerationErrors = "";
-      public static readonly String GithubRepoName = "azure-rest-api-specs";
-      // END: Code Generation Metadata Section
   }
 }
-
