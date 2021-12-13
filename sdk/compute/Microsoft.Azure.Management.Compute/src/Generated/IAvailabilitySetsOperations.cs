cb7df292b9ca7464221585d97e3af76dec54f1fa
SDKAuto <sdkautomation@microsoft.com>
2021-12-13 02:45:57 +0000

    CodeGen from PR 3012 in openapi-env-test/azure-rest-api-specs Merge 78b152452dcbabddb079185428356daf01e2f5ca into 184507b3a19ff48fe466c28902885aac66712711

--- sdk/compute/Microsoft.Azure.Management.Compute/src/Generated/IAvailabilitySetsOperations.cs
+++ sdk/compute/Microsoft.Azure.Management.Compute/src/Generated/IAvailabilitySetsOperations.cs
@@ -24,34 +24,6 @@ namespace Microsoft.Azure.Management.Compute
     public partial interface IAvailabilitySetsOperations
     {
         /// <summary>
-        /// Create or update an availability set.
-        /// </summary>
-        /// <param name='resourceGroupName'>
-        /// The name of the resource group.
-        /// </param>
-        /// <param name='availabilitySetName'>
-        /// The name of the availability set.
-        /// </param>
-        /// <param name='parameters'>
-        /// Parameters supplied to the Create Availability Set operation.
-        /// </param>
-        /// <param name='customHeaders'>
-        /// The headers that will be added to request.
-        /// </param>
-        /// <param name='cancellationToken'>
-        /// The cancellation token.
-        /// </param>
-        /// <exception cref="Microsoft.Rest.Azure.CloudException">
-        /// Thrown when the operation returned an invalid status code
-        /// </exception>
-        /// <exception cref="Microsoft.Rest.SerializationException">
-        /// Thrown when unable to deserialize the response
-        /// </exception>
-        /// <exception cref="Microsoft.Rest.ValidationException">
-        /// Thrown when a required parameter is null
-        /// </exception>
-        Task<AzureOperationResponse<AvailabilitySet>> CreateOrUpdateWithHttpMessagesAsync(string resourceGroupName, string availabilitySetName, AvailabilitySet parameters, Dictionary<string, List<string>> customHeaders = null, CancellationToken cancellationToken = default(CancellationToken));
-        /// <summary>
         /// Update an availability set.
         /// </summary>
         /// <param name='resourceGroupName'>
