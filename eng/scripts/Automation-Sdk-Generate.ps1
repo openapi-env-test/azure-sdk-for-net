0883f1d6912c3cb2573d3f588f3bb7728b7f44c2
Feng Zhou <55177366+fengzhou-msft@users.noreply.github.com>
2022-01-06 15:09:18 +0800

    Update Automation-Sdk-Generate.ps1

--- eng/scripts/Automation-Sdk-Generate.ps1
+++ eng/scripts/Automation-Sdk-Generate.ps1
@@ -50,8 +50,11 @@ foreach ($path in $autorestFilesPath) {
 Write-Host "Updated autorest.md files for all the changed swaggers. `n"
 
 $packages = @()
-$dotnet = Join-Path $RepoRoot "../.dotnet"
+$dotnet = Join-Path $RepoRoot ".dotnet"
+ls $dotnet | Write-Host
 $env:PATH = "$dotnet`:" + $env:PATH
+Write-Host $env:PATH
+dotnet --version | Write-Host
 foreach ($sdkPath in $sdksInfo.Keys) {
   $packageName = Split-Path $sdkPath -Leaf
   Write-Host "Generating code for " $packageName
