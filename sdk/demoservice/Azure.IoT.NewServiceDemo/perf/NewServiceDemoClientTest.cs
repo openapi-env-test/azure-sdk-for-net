a20762cce1d31e82bc832582b916e31034ba9834
azure-sdk <no-reply@azure-sdk.com>
2022-07-11 15:04:17 +0000

    [auto]create by sdk generation

---
+++ sdk/demoservice/Azure.IoT.NewServiceDemo/perf/NewServiceDemoClientTest.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Identity;
+using Azure.Test.Perf;
+using CommandLine;
+
+namespace Azure.IoT.NewServiceDemo.Perf
+{
+    public class NewServiceDemoClientTest : PerfTest<NewServiceDemoClientTest.NewServiceDemoClientPerfOptions>
+    {
+        /* please refer to https://github.com/Azure/azure-sdk-for-net/blob/main/sdk/template/Azure.Template/perf/TemplateClientTest.cs to write perf test. */
+
+        public NewServiceDemoClientTest(NewServiceDemoClientPerfOptions options) : base(options)
+        {
+        }
+        public class NewServiceDemoClientPerfOptions : PerfOptions
+        {
+        }
+
+        public override void Run(CancellationToken cancellationToken)
+        {
+        }
+
+        public override async Task RunAsync(CancellationToken cancellationToken)
+        {
+            await Task.Run(() =>
+            {
+                Console.WriteLine("exec some async operation");
+            });
+        }
+    }
+}
