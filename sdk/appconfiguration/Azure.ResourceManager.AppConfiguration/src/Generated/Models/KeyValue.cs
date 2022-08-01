6cd09c9e5335237987c57c82391c05a7979a7a73
SDKAuto <sdkautomation@microsoft.com>
2022-08-01 07:32:48 +0000

    CodeGen from PR 2506 in test-repo-billy/azure-rest-api-specs Merge eb097a5c4cb31fbc16164d7b409dae56d51286d2 into 161f2cd5033ba059e92a956b87278baf7461823a

--- sdk/appconfiguration/Azure.ResourceManager.AppConfiguration/src/Generated/Models/KeyValue.cs
+++ sdk/appconfiguration/Azure.ResourceManager.AppConfiguration/src/Generated/Models/KeyValue.cs
@@ -8,24 +8,19 @@
 using System;
 using System.Collections.Generic;
 using Azure.Core;
-using Azure.ResourceManager.Models;
 
-namespace Azure.ResourceManager.AppConfiguration
+namespace Azure.ResourceManager.AppConfiguration.Models
 {
-    /// <summary> A class representing the KeyValue data model. </summary>
-    public partial class KeyValueData : ResourceData
+    /// <summary> The result of a request to retrieve a key-value from the specified configuration store. </summary>
+    public partial class KeyValue
     {
-        /// <summary> Initializes a new instance of KeyValueData. </summary>
-        public KeyValueData()
+        /// <summary> Initializes a new instance of KeyValue. </summary>
+        internal KeyValue()
         {
             Tags = new ChangeTrackingDictionary<string, string>();
         }
 
-        /// <summary> Initializes a new instance of KeyValueData. </summary>
-        /// <param name="id"> The id. </param>
-        /// <param name="name"> The name. </param>
-        /// <param name="resourceType"> The resourceType. </param>
-        /// <param name="systemData"> The systemData. </param>
+        /// <summary> Initializes a new instance of KeyValue. </summary>
         /// <param name="key">
         /// The primary identifier of a key-value.
         /// The key is used in unison with the label to uniquely identify a key-value.
@@ -46,7 +41,7 @@ namespace Azure.ResourceManager.AppConfiguration
         /// A locked key-value may not be modified until it is unlocked.
         /// </param>
         /// <param name="tags"> A dictionary of tags that can help identify what a key-value may be applicable for. </param>
-        internal KeyValueData(ResourceIdentifier id, string name, ResourceType resourceType, SystemData systemData, string key, string label, string value, string contentType, string eTag, DateTimeOffset? lastModified, bool? locked, IDictionary<string, string> tags) : base(id, name, resourceType, systemData)
+        internal KeyValue(string key, string label, string value, string contentType, string eTag, DateTimeOffset? lastModified, bool? locked, IReadOnlyDictionary<string, string> tags)
         {
             Key = key;
             Label = label;
@@ -69,12 +64,12 @@ namespace Azure.ResourceManager.AppConfiguration
         /// </summary>
         public string Label { get; }
         /// <summary> The value of the key-value. </summary>
-        public string Value { get; set; }
+        public string Value { get; }
         /// <summary>
         /// The content type of the key-value&apos;s value.
         /// Providing a proper content-type can enable transformations of values when they are retrieved by applications.
         /// </summary>
-        public string ContentType { get; set; }
+        public string ContentType { get; }
         /// <summary> An ETag indicating the state of a key-value within a configuration store. </summary>
         public string ETag { get; }
         /// <summary> The last time a modifying operation was performed on the given key-value. </summary>
@@ -85,6 +80,6 @@ namespace Azure.ResourceManager.AppConfiguration
         /// </summary>
         public bool? Locked { get; }
         /// <summary> A dictionary of tags that can help identify what a key-value may be applicable for. </summary>
-        public IDictionary<string, string> Tags { get; }
+        public IReadOnlyDictionary<string, string> Tags { get; }
     }
 }
