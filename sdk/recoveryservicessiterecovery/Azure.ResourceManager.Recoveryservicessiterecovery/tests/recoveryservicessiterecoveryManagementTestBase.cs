1c736799b0ca03764b0d7ee3556680a70d2f65c5
SDKAuto <sdkautomation@microsoft.com>
2022-11-17 06:41:20 +0000

    CodeGen from PR 2751 in test-repo-billy/azure-rest-api-specs Merge 23e2337a0979ad4dacc8e6ae5b8760d74e74fe48 into 89c2c7c043047d12f9606c73446c4f790fb1f023

---
+++ sdk/recoveryservicessiterecovery/Azure.ResourceManager.Recoveryservicessiterecovery/tests/recoveryservicessiterecoveryManagementTestBase.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core;
+using Azure.Core.TestFramework;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.TestFramework;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace Azure.ResourceManager.Recoveryservicessiterecovery.Tests
+{
+    public class recoveryservicessiterecoveryManagementTestBase : ManagementRecordedTestBase<recoveryservicessiterecoveryManagementTestEnvironment>
+    {
+        protected ArmClient Client { get; private set; }
+
+        protected recoveryservicessiterecoveryManagementTestBase(bool isAsync, RecordedTestMode mode)
+        : base(isAsync, mode)
+        {
+        }
+
+        protected recoveryservicessiterecoveryManagementTestBase(bool isAsync)
+            : base(isAsync)
+        {
+        }
+
+        [SetUp]
+        public void CreateCommonClient()
+        {
+            Client = GetArmClient();
+        }
+
+        protected async Task<ResourceGroupResource> CreateResourceGroup(SubscriptionResource subscription, string rgNamePrefix, AzureLocation location)
+        {
+            string rgName = Recording.GenerateAssetName(rgNamePrefix);
+            ResourceGroupData input = new ResourceGroupData(location);
+            var lro = await subscription.GetResourceGroups().CreateOrUpdateAsync(WaitUntil.Completed, rgName, input);
+            return lro.Value;
+        }
+    }
+}
