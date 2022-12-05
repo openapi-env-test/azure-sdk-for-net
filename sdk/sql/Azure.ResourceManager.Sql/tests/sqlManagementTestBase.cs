15340016c2c052c3a57128dc02695843cd7ef537
SDKAuto <sdkautomation@microsoft.com>
2022-12-05 09:11:09 +0000

    CodeGen from PR 2753 in test-repo-billy/azure-rest-api-specs Merge c53f693ddf28a7997192ad917747504b3229cfe2 into 7536f4a55781077b3e33dda332495e2ef3346254

---
+++ sdk/sql/Azure.ResourceManager.Sql/tests/sqlManagementTestBase.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core;
+using Azure.Core.TestFramework;
+using Azure.ResourceManager.Resources;
+using Azure.ResourceManager.TestFramework;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace Azure.ResourceManager.Sql.Tests
+{
+    public class sqlManagementTestBase : ManagementRecordedTestBase<sqlManagementTestEnvironment>
+    {
+        protected ArmClient Client { get; private set; }
+
+        protected sqlManagementTestBase(bool isAsync, RecordedTestMode mode)
+        : base(isAsync, mode)
+        {
+        }
+
+        protected sqlManagementTestBase(bool isAsync)
+            : base(isAsync)
+        {
+        }
+
+        [SetUp]
+        public void CreateCommonClient()
+        {
+            Client = GetArmClient();
+        }
+
+        protected async Task<ResourceGroupResource> CreateResourceGroup(SubscriptionResource subscription, string rgNamePrefix, AzureLocation location)
+        {
+            string rgName = Recording.GenerateAssetName(rgNamePrefix);
+            ResourceGroupData input = new ResourceGroupData(location);
+            var lro = await subscription.GetResourceGroups().CreateOrUpdateAsync(WaitUntil.Completed, rgName, input);
+            return lro.Value;
+        }
+    }
+}
